0a12b76ee93bf0aa3a0c8263ff2b709e65fd59a6
Clinton Johnson <dragoon1246224@gmail.com>
2018-09-10 10:32:10 -0400

    Two Done

---
+++ script.js
@@ -0,0 +1,35 @@
+//Katas1 forEach()
+function forEach(array, callBackFunction) {
+    for (let x = 0; x < array.length; x++) {
+        const currentValue = array[x]
+        callBackFunction(currentValue, x, array)
+    }
+}
+
+//Katas2 map()
+function map(array, callbackFunction) {
+    for (let x = 0; x < array.length; x++) {
+        const newArray = array[x]
+        callBackFunction(newArray, x, array)
+    }
+}
+
+//Katas3 some()
+function some(array, callBackFunction) {
+    for (let x = o; x < array.length; x++) {
+        const 
+    }
+}
+
+//Katas4 find()
+
+
+//Katas5 findIndex()
+
+
+//Katas6 every()
+
+
+//Katas7 filter()
+
+
